6ba0d2d55f2d3db2bc39adb6a2f77a4e9035b461
Jesseadriano10 <jesseadrian@hotmail.com>
2024-01-11 15:36:13 -0600

    Upload current version of deliverable1 part1

--- Tutorials/tutorial2/jaa369_tutorial2-led.py
+++ Tutorials/tutorial2/jaa369_tutorial2-led.py
@@ -1,4 +1,4 @@
-import Rpi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 # Set hexadecimal values for each color
@@ -9,28 +9,55 @@ import time
 # 0x0000 = off
 colors = [0xFF00, 0x00FF, 0x0FF0, 0xF00F, 0x0000]
 # Pins 17, 18, 21 are connected to RGB led, 21 is a switch
-pins = (17, 18, 21, 27)
+pins = (17, 18, 27, 25) # Blue, Green, Red, Switch
+rgb_B = 17
+rgb_G = 18
+rgb_R = 27
+
+
 
 def setup(pins):
-    GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
-    GPIO.setup(pins[0:2], GPIO.OUT) # Set all LED pins mode as output
-    GPIO.output(pins[0:2], GPIO.LOW) # Set all pins to LOW(0v) to turn off LEDs
+    GPIO.setmode(GPIO.BCM) # Numbers GPIOs by physical location
+    for i in range(len(pins)-1):
+        print("Configuring pin: " + str(pins[i]))
+        GPIO.setup(pins[i], GPIO.OUT) # Set all LED pins mode as output
+        GPIO.output(pins[i], GPIO.HIGH) # Set all LED pins to high(+3.3V) to off led
     GPIO.setup(pins[3], GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set button pin mode as input pull-up to HIGH level(3.3V)
-    GPIO.add_event_detect(pins[3], GPIO.FALLING, callback=loop, bouncetime=200) # wait for falling
+    GPIO.add_event_detect(pins[3], GPIO.BOTH, callback=detect, bouncetime=200) # wait for falling
 
 def detect(chn):
-    print('Button pressed')
     Led(GPIO.input(pins[3]))
+
+def loop():
+    while True:
+        pass
+
+
+def destroy():
+    for i in range(len(pins) - 1):
+        GPIO.output(pins[i], GPIO.LOW) # Turn off all LEDs
+    GPIO.cleanup() # Release resource
 
 # Each time the button is pressed, the LED will change color based on list
 def Led(x):
-    for i in range(len(colors)):
-        GPIO.output(pins[0], (colors[i]&0xFF0000)>>16)
-        GPIO.output(pins[1], (colors[i]&0x00FF00)>>8)
-        time.sleep(0.5)
-    GPIO.output(pins[0:2], GPIO.LOW)
+    if x == 0:
+        print("Button pressed")
+        for i in range(len(colors)):
+            GPIO.output(rgb_R, (colors[i] & 0x1000) >> 12)
+            GPIO.output(rgb_G, (colors[i] & 0x0100) >> 8)
+            GPIO.output(rgb_B, (colors[i] & 0x0010) >> 4)
+            time.sleep(0.5)
+    else:
+        print("Button released")
+        for i in range(len(pins) - 1):
+            GPIO.output(pins[i], GPIO.LOW)
+        
     
 
-
-def loop():
+    
+if __name__ == '__main__': # Program starts from here
     setup(pins)
+    try:
+        loop()
+    except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
+        destroy()
