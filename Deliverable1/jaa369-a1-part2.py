2ebcaa2273bfe96004abc5cdd58492ffe01cc248
Patrick Sampson <34658202+Jesseadriano10@users.noreply.github.com>
2024-01-12 19:37:38 -0600

    Refactoring jaa369-a1-part2.py
    Organizing Imports: Grouping related imports together for clarity.
    Using Constants: Using uppercase for constants like pin numbers and ADC address.
    Improving Function Names: Renaming functions for better understanding of their purpose.
    Refactoring LED Control Logic: Simplifying the LED control logic.
    Removing Global Variables: Reducing the use of global variables for better practice.
    Enhancing Comments: Making comments more informative.

--- Deliverable1/jaa369-a1-part2.py
+++ Deliverable1/jaa369-a1-part2.py
@@ -6,26 +6,44 @@ import RPi.GPIO as GPIO
 import time
 import PCF8591 as ADC
 
-pins = (17, 18, 27, 25) # Blue, Green, Red, Switch
-rgb_B = pins[0]
-rgb_G = pins[1]
-rgb_R = pins[2]
-switch = pins[3]
-colors = (rgb_B, rgb_G, rgb_R)
-def setup(pins):
-    GPIO.setmode(GPIO.BCM) # Numbers GPIOs by physical location
-    # LED CONFIG
-    for i in range(len(pins)-1):
-        print("Configuring pin: " + str(pins[i]))
-        GPIO.setup(pins[i], GPIO.OUT) # Set all LED pins mode as output
-    # SWITCH CONFIG
-    GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set button pin mode as input pull-up to HIGH level(3.3V)
-    GPIO.add_event_detect(switch, GPIO.BOTH, callback=detect) # wait for falling
-    # Configure PCF8591 ADC
+# Constants
+PINS = (17, 18, 27, 25)  # Blue, Green, Red, Switch
+RGB_B, RGB_G, RGB_R, SWITCH = PINS
+ADC_ADDRESS = 0x48
+VOICE_THRESHOLD = 80
+VOICE_MIDDLE = 120
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    for pin in PINS[:-1]:
+        print(f"Configuring pin: {pin}")
+        GPIO.setup(pin, GPIO.OUT)
+    
+    GPIO.setup(SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(SWITCH, GPIO.BOTH, callback=button_pressed)
+    
     print("Configuring ADC")
-    ADC.setup(0x48)
-def detect(chn):
-    Led(GPIO.input(switch))
+    ADC.setup(ADC_ADDRESS)
+
+def button_pressed(channel):
+    global voice_enabled
+    voice_enabled = GPIO.input(SWITCH)
+
+def update_leds(voice_val):
+    if voice_val < VOICE_THRESHOLD:
+        print("Too Loud!")
+        set_led_color(False, False, True)  # Yellow
+    elif VOICE_THRESHOLD <= voice_val < VOICE_MIDDLE:
+        print("Just Right!")
+        set_led_color(False, True, False)  # Purple
+    else:
+        print("Dead Quiet!")
+        set_led_color(True, False, False)  # Blue
+
+def set_led_color(red, green, blue):
+    GPIO.output(RGB_R, GPIO.LOW if red else GPIO.HIGH)
+    GPIO.output(RGB_G, GPIO.LOW if green else GPIO.HIGH)
+    GPIO.output(RGB_B, GPIO.LOW if blue else GPIO.HIGH)
 
 def loop():
     global voice_enabled
@@ -33,57 +51,18 @@ def loop():
     while True:
         if voice_enabled:
             voice_val = ADC.read(0)
-            if voice_val:
-                print("Voice val is: ", voice_val)
-                if voice_val < 80:
-                    print("Too Loud!")
-                    # Turn Yellow
-                    GPIO.output(rgb_R, GPIO.LOW)
-                    GPIO.output(rgb_G, GPIO.LOW)
-                    GPIO.output(rgb_B, GPIO.HIGH)
-                elif voice_val < 120 and voice_val > 80:
-                    print("Just Right!")
-                    # Turn Purple
-                    GPIO.output(rgb_R, GPIO.LOW)
-                    GPIO.output(rgb_G, GPIO.HIGH)
-                    GPIO.output(rgb_B, GPIO.LOW)
-                else:
-                    print("Dead Quiet!")
-                    # Turn Blue
-                    GPIO.output(rgb_R, GPIO.HIGH)
-                    GPIO.output(rgb_G, GPIO.LOW)
-                    GPIO.output(rgb_B, GPIO.LOW)
-                time.sleep(0.2)
-        
-        
-        
-
+            print(f"Voice val is: {voice_val}")
+            update_leds(voice_val)
+            time.sleep(0.2)
 
 def destroy():
-    for i in range(len(pins) - 1):
-        GPIO.output(pins[i], GPIO.LOW) # Turn off all LEDs
-    
-    GPIO.cleanup() # Release resource
+    for pin in PINS[:-1]:
+        GPIO.output(pin, GPIO.LOW)
+    GPIO.cleanup()
 
-# Each time the button is pressed, the LED will change color based on list
-def Led(x):
-    global voice_enabled
-    if x == 0:
-        # Whenever the button is pressed, we will allow voice detection
-        voice_enabled = True
-    else:
-        voice_enabled = False
-        print("Press the button to enable voice detection!")
-        # Turn LED Red
-        GPIO.output(rgb_R, GPIO.LOW)
-        GPIO.output(rgb_G, GPIO.HIGH)
-        GPIO.output(rgb_B, GPIO.HIGH)
-    
-
-    
-if __name__ == '__main__': # Program starts from here
-    setup(pins)
+if __name__ == '__main__':
+    setup()
     try:
         loop()
-    except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be executed.
+    except KeyboardInterrupt:
         destroy()
